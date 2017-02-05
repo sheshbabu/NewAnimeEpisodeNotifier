84a25b616927965c0f79db536640be7fda733ff1
Sheshbabu <sheshbabu@redmart.com>
2017-02-05 12:13:10 +0800

    Removed app header

--- components/App.js
+++ components/App.js
@@ -2,13 +2,11 @@
 
 import React from 'react';
 import { View } from 'react-native';
-import AppHeader from './AppHeader';
 import AnimeList from './AnimeList';
 
 export default function App () {
     return (
         <View>
-            <AppHeader/>
             <AnimeList/>
         </View>
     );
